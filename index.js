c8f8650c77be75ff9dbadf6b06f9c5c7e7f2b68b
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-11-19 08:25:47 +0530

    pal

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-var pal=["mom","js","malayalam","html","css","dad"]
+// var a="mom"
+// var b=""
+// for(i=a.length-1;i>=0;i--){
+//     b=b+a[i]
+// }
 
-j
+// if(a==b){
+//     console.log("yes");
+
+// }else{ console.log("no");
+// }
+
+var aos = ["mom", "js", "malayalam", "html", "css", "dad"];
+var op = [];
+for (let i = 0; i < aos.length; i++) {
+  let word = aos[i];
+  if (word == word.split("").reverse().join("")) {
+    op.push(word);
+  }
+}
+console.log(op);//[ 'mom', 'malayalam', 'dad' ]
